d2a151b3740f998193eca2766db3a3c42d642f17
Fayerling <Fayerling@foxmail.com>
2026-06-29 19:46:27 +0800

    Fix: move robots directives to googleBot object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,9 +41,13 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
-    'max-snippet': -1,
-    'max-image-preview': 'large',
-    'max-video-preview': -1,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-snippet': -1,
+      'max-image-preview': 'large',
+      'max-video-preview': -1,
+    },
   },
 };
 
